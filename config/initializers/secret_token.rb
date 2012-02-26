a2da4f19a7b2200d8bdf39e9095abb8fe27adff0
acadavid <acadavid@gmail.com>
2012-02-26 15:44:02 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teatroenmedellin::Application.config.secret_token = '782a65a22817e22ebf69b395a520b70093c3315f67bac82c12e1cfb9f573bd6224579d5ea3f7716e4dc5115bf200700eb8dd550b42f54d34eb8fbaea25ede9ac'
