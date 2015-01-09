71750f4a45e01e7593ee2483909bf69283a46a60
Andres Martinez Gutierrez <amartinez0000@gmail.com>
2015-01-09 18:24:59 -0500

    Kaminari gem added for pagination

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'therubyracer', :require => 'v8'
 gem 'dalli'
 gem 'turbolinks'
 gem 'sorcery'
+gem 'kaminari'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,10 @@ class HomeController < ApplicationController
   respond_to :html
 
   def index
-    date = params[:date].nil? ? Date.today : Date.parse(params[:date])
-    @acts_date = ActDate.when(date)
+    @date = params[:date].nil? ? Date.today : Date.parse(params[:date])
+    @act_dates = ActDate.page(params[:page]).per(5).when(@date)
 
-    @dates = (Date.today..date+5).to_a
+    @dates = (Date.today..@date+5).to_a
 
     respond_with @acts_date
   end
